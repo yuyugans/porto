84de12c15df4adf1471663bf46f77ccd6b448549
yuyugans <mohamadyusupaxioo@gmail.com>
2023-12-17 11:13:55 +0700

    first

---
+++ main.js
@@ -0,0 +1,17 @@
+//togle
+const navslide = ()=>{
+    const burger = document.querySelector(".burger");
+    const navList = document.querySelector("nav");
+    burger.addEventListener("click", () =>{
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger")
+    })
+}
+
+navslide()
+
+window.onbeforeunload = ()  =>{
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
